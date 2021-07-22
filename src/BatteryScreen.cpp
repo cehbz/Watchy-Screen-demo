0cf2436febc6a16659ca8bddae8bcfa36e5cb458
Charles Haynes <ceh+github@ceh.bz>
2021-07-22 23:08:38 +1000

    make pretty icons

--- src/BatteryScreen.cpp
+++ src/BatteryScreen.cpp
@@ -1,13 +1,19 @@
 #include "BatteryScreen.h"
 
-#include "BluetoothScreen.h"
-#include "OptimaLTStd12pt7b.h"
-#include "ShowBatteryScreen.h"
+#include "Adafruit_GFX.h"
+#include "OptimaLTStd22pt7b.h"
 #include "Watchy.h"
-#include "WeatherScreen.h"
+#include "icons.h"
 
 void BatteryScreen::show() {
-  Watchy::display.setFont(&OptimaLTStd12pt7b);
-  Watchy::display.setCursor(0, 0);
-  Watchy::display.printf("\nbattery\n");
+  constexpr char *label = "battery";
+  const uint16_t fgColor = (bgColor == GxEPD_WHITE ? GxEPD_BLACK : GxEPD_WHITE);
+  Watchy::display.setFont(&OptimaLTStd22pt7b);
+  int16_t x1, y1;
+  uint16_t w, h;
+  Watchy::display.drawBitmap((200 - 96) / 2, (200 - 96) / 2 / 2, bitmap_battery,
+                             96, 96, fgColor);
+  Watchy::display.getTextBounds(label, 0, 0, &x1, &y1, &w, &h);
+  Watchy::display.setCursor((200 - w) / 2, 170);
+  Watchy::display.print(label);
 }
--- src/OrientationScreen.cpp
+++ src/OrientationScreen.cpp
@@ -1,13 +1,19 @@
 #include "OrientationScreen.h"
 
-#include "BluetoothScreen.h"
-#include "OptimaLTStd12pt7b.h"
-#include "SettingsScreen.h"
-#include "ShowOrientationScreen.h"
+#include "Adafruit_GFX.h"
+#include "OptimaLTStd22pt7b.h"
 #include "Watchy.h"
+#include "icons.h"
 
 void OrientationScreen::show() {
-  Watchy::display.setFont(&OptimaLTStd12pt7b);
-  Watchy::display.setCursor(0, 0);
-  Watchy::display.printf("\norientation\n");
+  constexpr const char *label = "orientation";
+  const uint16_t fgColor = (bgColor == GxEPD_WHITE ? GxEPD_BLACK : GxEPD_WHITE);
+  Watchy::display.setFont(&OptimaLTStd22pt7b);
+  int16_t x1, y1;
+  uint16_t w, h;
+  Watchy::display.drawBitmap((200 - 96) / 2, (200 - 96) / 2 / 2,
+                             bitmap_orientation, 96, 96, fgColor);
+  Watchy::display.getTextBounds(label, 0, 0, &x1, &y1, &w, &h);
+  Watchy::display.setCursor((200 - w) / 2, 170);
+  Watchy::display.print(label);
 }
